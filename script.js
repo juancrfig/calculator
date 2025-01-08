afc27f42e698485788df57ce7b4178a612184e4f
juancrfig <juancrfig@gmail.com>
2025-01-08 07:55:40 -0500

    Add basic functionality to operators buttons

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const screen = document.querySelector('.screen');
 const numbers = document.querySelectorAll('.number');
 const deleteButton = document.querySelector('.erase');
+const addButton = document.querySelector('.addition');
+const subButton = document.querySelector('.substraction');
+const multButton = document.querySelector('.multiplication');
+const divButton = document.querySelector('.division');
+const arrayOperators = ['+', '-', '*', '÷']
 
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
@@ -10,4 +15,43 @@ numbers.forEach((number) => {
 
 deleteButton.addEventListener('click', () => {
     screen.textContent = screen.textContent.slice(0, -1);
+})
+
+addButton.addEventListener('click', () => {
+    if (checkForOperators()) {
+    } else {
+        screen.textContent+= '+';
+    };
+})
+
+function checkForOperators() {
+    const length = screen.textContent.length;
+    if (arrayOperators.includes(screen.textContent[length -1])) {
+        return true;
+    } else if (screen.textContent === '') {
+        return true
+    } else {
+        return false;
+    }
+}
+
+subButton.addEventListener('click', () => {
+    if (checkForOperators()) {
+    } else {
+        screen.textContent+= '-';
+    };
+})
+
+multButton.addEventListener('click', () => {
+    if (checkForOperators()) {
+    } else {
+        screen.textContent+= '×';
+    };
+})
+
+divButton.addEventListener('click', () => {
+    if (checkForOperators()) {
+    } else {
+        screen.textContent+= '÷';
+    };
 })
