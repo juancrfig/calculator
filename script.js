8995a275b111449b7953748926e021050142f6ad
juancrfig <juancrfig@gmail.com>
2025-01-07 23:15:23 -0500

    Add erase functionality

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const screen = document.querySelector('.screen');
 const numbers = document.querySelectorAll('.number');
+const deleteButton = document.querySelector('.erase');
 
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
         screen.textContent += number.classList[2];
     })
+})
+
+deleteButton.addEventListener('click', () => {
+    screen.textContent = screen.textContent.slice(0, -1);
 })
